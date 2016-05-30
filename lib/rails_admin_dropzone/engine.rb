dd20ae941435c33bea0a0096ce6efd974c1d74dd
Luiz Picolo <luizpicolo@gmail.com>
2016-05-30 16:28:53 -0400

    FIXED: Error with multiples customs assets;
     - Removed rails_admin custom assets;
     - Added custom assets in view;

--- lib/rails_admin_dropzone/engine.rb
+++ lib/rails_admin_dropzone/engine.rb
@@ -4,5 +4,11 @@ require 'jquery-rails'
 
 module RailsAdminDropzone
   class Engine < ::Rails::Engine
+    initializer 'rails_admin_dropzone_assets' do |app|
+      app.config.assets.precompile += %w(
+        rails_admin/rails_admin_dropzone.js
+        rails_admin/rails_admin_dropzone.css
+      )
+    end
   end
 end
